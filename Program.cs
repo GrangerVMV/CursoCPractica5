55bd322ccc4673e3bd92bf72a18ffd30cb71d443
GrangerVMV <virginia.marchena@gmail.com>
2022-09-22 14:26:06 +0200

    Tercer ejemplo: Excepción genérica y mensaje descriptivo

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CursoCPractica5_Excepciones
     {
         static void Main(string[] args)
         {
-            // Primer ejemplo: Excepción de formato
+            
             Random referenciaARandom = new Random();
             int numeroAleatorio = referenciaARandom.Next(0, 100);
             Console.WriteLine("Por favor, elige un número del 0-100");
@@ -19,14 +19,23 @@ namespace CursoCPractica5_Excepciones
                 {
                     numeroElegido = int.Parse(Console.ReadLine());
                 }
-                catch (FormatException ex)
+                /*catch (FormatException ex) // Primer ejemplo: Excepción de formato
                 {
                     Console.WriteLine("El valor introducido no es válido. El programa tomará como valor el 0");
                     numeroElegido = 0;
                 }
                 catch (OverflowException ex2) //Segundo ejemplo: Excepción de desbordamiento. Añadimos otro catch para tratar el error del desbordamiento
                 {
-                    Console.WriteLine("El valor introducido no demasiado alto. El programa tomará como valor el 0");
+                    Console.WriteLine("El valor introducido es demasiado alto. El programa tomará como valor el 0");
+                    numeroElegido = 0;
+                }*/
+                //Tercer ejemplo: Excepción genérica y mensaje descriptivo.
+                //No es aconsejable usar este tipo de excepción (demasiado genérica). La expresión entre paréntesis puede omitirse pero no se aconseja
+                //La herencia sería Exception -> SystemException -> FormatException, OverflowException, etc
+                catch(Exception ex3)
+                {
+                    Console.WriteLine("El valor introducido no es válido. El programa tomará como valor el 0");
+                    Console.WriteLine(ex3.Message);
                     numeroElegido = 0;
                 }
                 if (numeroAleatorio > numeroElegido) Console.WriteLine("Vuelve a intentarlo, el número es mayor que el que has elegido");
